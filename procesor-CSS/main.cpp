5aaf168d985b4500d82674d1a0860de795cf65ad
ajgors <igor.stadnicki@gmail.com>
2023-03-19 21:52:59 +0100

    my string working

--- procesor-CSS/main.cpp
+++ procesor-CSS/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include "stringImp.h"
+#include "string.h"
 #define ROZ 8
+
 
 //sekcje jako listy 2 
 
+struct atrybut {
+	//String nazwa;
+	String nazwa;
+	String wartosc;
 
+
+	atrybut()
+		:nazwa(""), wartosc("")
+	{
+
+	}
+
+	atrybut(String nazwa, String wartosc)
+		:nazwa(nazwa), wartosc(wartosc)
+	{
+
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const atrybut& obj)
+	{
+		os << obj.nazwa/* << " " << obj.wartosc*/;
+		return os;
+	}
+
+};
 
 template<typename T>
 class List {
@@ -14,6 +41,12 @@ private:
 public:
 
 
+	List()
+		: next(nullptr)
+	{
+	
+	}
+
 	List(T data) {
 		this->data = data;
 		this->next = nullptr;
@@ -21,6 +54,7 @@ public:
 
 	void add(T data) {
 		List* temp = this;
+
 		while (temp->next != nullptr) {
 			temp = temp->next;
 		}
@@ -28,7 +62,8 @@ public:
 	}
 
 	void print() {
-		List* temp = this;
+		List* temp = this->next;
+
 		while (temp != nullptr) {
 			std::cout << temp->data << " ";
 			temp = temp->next;
@@ -37,7 +72,7 @@ public:
 	}
 
 	int getLen() {
-		List* temp = this;
+		List* temp = this->next;
 		int len = 0;
 		while (temp != nullptr) {
 			len++;
@@ -45,33 +80,82 @@ public:
 		}
 		return len;
 	}
+
+	atrybut* findByNazwa(String nazwa) {
+		List* temp = this;
+		while (temp != nullptr) {
+			if (temp->data.nazwa == nazwa)
+				return &(temp->data);
+			temp = temp->next;
+		}
+		return nullptr;
+	}
 };
 
 
 
 class Block {
+	
+private: 
+	List<String> selektor;
+	List<atrybut> atrybuty;
 public:
-	List<std::string> selektor;
-	List<std::string> atrybuty;
+	
 	bool used;
 	Block()
-		: selektor(""), atrybuty(""), used(false)
+		:used(false)
 	{
 
 	}
 
-	Block(std::string selektor, std::string atrybut)
-		:selektor(selektor), atrybuty(atrybut), used(true)
+	Block(String selektor, String nazwa, String value)
+		:selektor(selektor), atrybuty(atrybut(nazwa,value)), used(true)
 	{
+	}
+
+	Block(const Block& b) {
+		selektor = b.selektor;
+		atrybuty = b.atrybuty;
+	}
+	
+	Block& operator=(const Block& b) {
+		selektor = b.selektor;
+		atrybuty = b.atrybuty;
+		return *this;
+	}
+
+	void addAtribute(atrybut a) {
+		//sprawdzic czy juz taki atrybut jest jak tak to zamienic
+
+		atrybut* k = atrybuty.findByNazwa(a.nazwa);
+		if (k) {
+			k->wartosc = a.wartosc;
+		}
+		else {
+			atrybuty.add(a);
+		}
+	}
+	void addSelektor(String selector) {
+		selektor.add(selector);
+	}
+
+	size_t getSelectorLen() {
+		return selektor.getLen();
+	}
+
+	void print() {
+		selektor.print();
+		atrybuty.print();
 	}
 };
 
 //template<typename T>
 class ListLinked {
 
+private:
+	size_t size;
 public:
 	Block* blocks;
-	size_t size;
 	ListLinked* next;
 	ListLinked* prev;
 
@@ -81,36 +165,86 @@ public:
 	}
 
 	void addBlock(Block block) {
-		if (size < ROZ) {
+		
+		ListLinked* temp = this;
+		
+		while (temp->size >= ROZ) {
+			if (temp->next == nullptr) {
+				ListLinked* t = new ListLinked();
+				t->prev = temp;
+				temp->next = t;
+				temp = t;
+			}
+			else {
+				temp = temp->next;
+			}
+		}
+		if (temp->size < ROZ) {
 			for (int i = 0; i < ROZ; i++) {
-				if (blocks[i].used == false) {
-					this->blocks[i] = block;
-					this->blocks[i].used = true;
-					this->size++;
+				if (temp->blocks[i].used == false) {
+					temp->blocks[i] = block;
+					temp->blocks[i].used = true;
+					temp->size++;
 					break;
 				}
 			}
-		}
-		else {
-			ListLinked* temp = new ListLinked();
-			temp->prev = this;
-			this->next = temp;
 		}
 
 	}
 
 	void printBlocks() {
-		for (int i = 0; i < ROZ; i++) {
-			if (blocks[i].used == true) {
-				blocks[i].selektor.print();
-				blocks[i].atrybuty.print();
+		ListLinked* temp = this;
+		while (temp != nullptr) {
+			for (int i = 0; i < ROZ; i++) {
+				if (temp->blocks[i].used == true) {
+					temp->blocks[i].print();
+				}
 			}
+			temp = temp->next;
 		}
 	}
 
 	~ListLinked() {
 		delete[] blocks;
 	}
+
+	size_t getSize() {
+		return size;
+	}
+
+	//liczba sekcji
+	size_t numberOfSections() {
+		size_t result = 0;
+
+		ListLinked* tmp = this;
+
+		while (tmp != nullptr) {
+			result += tmp->getSize();
+			tmp = tmp->next;
+		}
+
+		return result;
+	}
+
+	size_t numberOfSelectorsInSection(int n) {
+		int current = 0;
+		ListLinked* tmp = this;
+
+		while (n > ROZ) {
+			tmp = tmp->next;
+			n -= tmp->getSize();
+		}
+
+		for (int i = 0, k = 0; i < ROZ; i++) {
+			if (blocks[i].used == true) {
+				k++;
+			}
+			if (k == n) {
+				return blocks[k-1].getSelectorLen();
+			}
+		}
+		return 0;
+	};
 };
 
 
@@ -118,18 +252,23 @@ public:
 
 int main() {
 	ListLinked bloki;
-
 	bool selectors = true;
+
 	bool atributes = false;
+	bool commands = false;
+
 	while (true) {
 
-		std::string tmp;
+		String tmp;
 		Block block;
 		if (selectors) {
 			bool end = false;
 			block.used = true;
 			while (true) {
+				//char* buffor = new char[50];
+				//std::cin >> buffor;
 				std::cin >> tmp;
+				//tmp = buffor;
 				if (tmp == "{") {
 					selectors = false;
 					atributes = true;
@@ -145,20 +284,31 @@ int main() {
 					end = true;
 					break;
 				}
+				else if (tmp == "????") {
+					selectors = false;
+					atributes = false;
+					commands = true;
+					break;
+				}
 				else {
-					block.selektor.add(tmp);
+					if (tmp.at(tmp.length()-1) == ',') {
+						tmp.pop_back();
+					}
+					if (tmp != " ") {
+						block.addSelektor(tmp);
+					}
 				}
 			}
 			if (end) break;
 		}
 		if (atributes) {
-
-			std::string value;
+			String value;
 			bool running = true;
 			while (tmp != "}" && value != "}" && running ) {
+	
 				if (tmp != "") {
-					block.selektor.add(tmp.substr(1));
-					tmp = "";
+					//tmp = tmp.substr(1);
+					tmp.pop_back();
 				}
 				else {
 					std::cin >> tmp;
@@ -167,25 +317,41 @@ int main() {
 				}
 				std::cin >> value;
 				if (value == "}") break;
-				std::cout << value.back();
-				if (value.back() == '}') {
+				if (value.at(value.length() -1) == '}') {
 					value.pop_back();
 					running = false;
 				}
 				
 				value.pop_back();
-
-				block.atrybuty.add(tmp + " == " +  value);
+				if (tmp != "" && value != "") {
+					block.addAtribute(atrybut(tmp, value));
+				}
 				tmp = "";
 			}
 			bloki.addBlock(block);
 			atributes = false;
 			selectors = true;
 		}
+		if (commands) {
+			std::cin >> tmp;
+			if (tmp == "?") {
+				std::cout << "? == " << bloki.numberOfSections() << std::endl;
+			}
+			else if (tmp == "****") {
+				break;
+			}
+			else if (tmp.is_in("S,?")) {
+				int n;
+				tmp.cut(tmp.length() - 4); //remove S,?
+				n = atoi(tmp.c_str());
+				size_t result = bloki.numberOfSelectorsInSection(n);
+				std::cout << n << ",S,? == " << result << std::endl;
+			}
+			
+		}
 
 	}
 	bloki.printBlocks();
-
 	
 	return 0;
 }
---
+++ procesor-CSS/stringImp.h
@@ -0,0 +1,240 @@
+#pragma once
+#include <iostream>
+
+//Implementacja Stringa
+class String {
+
+private:
+	size_t m_length;
+	char* m_string;
+
+public:
+
+	String()
+		:m_length(0), m_string(nullptr)
+	{
+	}
+
+	String& operator=(const String& string) {
+
+		m_length = string.m_length;
+		delete[] m_string;
+		m_string = new char[m_length + 1];
+		//strcpy_s(m_string, m_length + 1, string.m_string);
+
+		for (int i = 0; i < m_length; i++) {
+			m_string[i] = string.m_string[i];
+		}
+		m_string[m_length] = '\0';
+
+		return *this;
+	}
+
+	String& operator=(const char* string) {
+		m_length = strlen(string);
+		delete[] m_string;
+		m_string = new char[m_length + 1];
+		strcpy_s(m_string, m_length + 1, string);
+
+		return *this;
+	}
+	
+	
+	//nowe
+	
+	bool operator==(String& other) {
+		//if (m_string == NULL) return false;
+		return strcmp(m_string, other.m_string) == 0;
+	}
+
+	bool operator==(const char* other) {
+		return strcmp(m_string, other) == 0;
+	}
+
+	bool operator!=(const char* other) {
+		if (m_length == 0 && strlen(other) > 0) return true;
+
+		return strcmp(m_string, other) != 0;
+	}
+	
+
+	void pop_back() {
+		if (m_length > 0) {
+			m_length--;
+			char* new_string = new char[m_length + 1];
+			memcpy(new_string, m_string, m_length);
+			new_string[m_length] = '\0';
+			delete[] m_string;
+			m_string = new_string;
+		}
+	}
+
+
+	String(const char* string)
+		:m_length(strlen(string))
+	{
+		m_string = new char[m_length + 1];
+		memcpy(m_string, string, m_length + 1);
+	}
+
+	~String() {
+		delete[] m_string;
+	}
+
+	//copy constructor
+	String(const String& other)
+
+	{
+		m_length = other.m_length;
+		m_string = new char[other.m_length + 1];
+
+		if (m_length > 0) {
+			memcpy(m_string, other.m_string, m_length + 1);
+		}
+	}
+
+
+	char& operator[](size_t index) {
+		return m_string[index];
+	}
+
+	void print() {
+		std::cout << m_string << std::endl;
+	}
+
+	String& append(const char* to_append) {
+
+		size_t length = strlen(to_append);
+
+		size_t new_length = m_length + length + 1;
+
+		char* new_string = new char[new_length];
+		std::memset(new_string, 0, sizeof(char) * new_length);
+
+		if (new_string) {
+
+			if (m_length > 0) {
+				strcat_s(new_string, sizeof(char) * new_length, m_string);
+			}
+			strcat_s(new_string, sizeof(char) * new_length, to_append);
+			delete[] m_string;
+			m_string = new_string;
+			m_length = new_length;
+		}
+
+		return *this;
+	}
+
+	String& operator+(const char* to_append) {
+		return append(to_append);
+	}
+
+	size_t length() {
+		return m_length;
+	}
+
+	size_t find_substring(const char* substring) {
+		size_t length = strlen(substring);
+
+		size_t cout = 0;
+		for (size_t i = 0; i < m_length; i++) {
+			for (size_t k = 0; k < length; k++) {
+				if (m_string[i + k] == substring[k]) cout++;
+				else {
+					i += cout;
+					cout = 0;
+					break;
+				}
+				if (cout == length) return i;
+			}
+		}
+
+		return -1;
+	}
+
+	bool is_in(const char* substring) {
+		return find_substring(substring) >= 0;
+	}
+
+	void toUpperCase() {
+		for (size_t i = 0; i < m_length; i++) {
+			m_string[i] = toupper(m_string[i]);
+		}
+	}
+	void toLowerCase() {
+		for (size_t i = 0; i < m_length; i++) {
+			m_string[i] = tolower(m_string[i]);
+		}
+	}
+
+	//from index to end
+	void slice(size_t index) {
+		size_t new_length = m_length - index;
+
+		char* new_string = new char[new_length+1];
+		std::memset(new_string, 0, new_length+1);
+
+		if (new_string) {
+			strncat_s(new_string, new_length+1, m_string + index, new_length);
+			delete[] m_string;
+			m_string = new_string;
+			m_length = new_length;
+		}
+	}
+
+	//from 0 to index
+	void cut(size_t index) {
+		size_t new_length = index;
+
+		char* new_string = new char[new_length+1];
+		std::memset(new_string, 0, new_length+1);
+
+		if (new_string) {
+			strncat_s(new_string, new_length+1, m_string, new_length);
+			delete[] m_string;
+			m_string = new_string;
+			m_length = new_length;
+		}
+
+	}
+
+	//substring
+	
+	char at(size_t index) {
+		return m_string[index];
+	}
+
+	char* c_str() {
+		return m_string;
+	}
+
+	void clear() {
+		delete[] m_string;
+		m_string = new char[1];
+		m_length = 1;
+	}
+	//friend pozwala funkcji ktora pobiera obiekt miec dostêp do prywatnych pól
+	friend std::ostream& operator<<(std::ostream& os, const String& string);
+	friend std::istream& operator>>(std::istream& is, String& out);
+
+};
+
+
+std::ostream& operator<<(std::ostream& os, const String& string)
+{
+	os << string.m_string;
+	return os;
+}
+
+
+std::istream& operator>>(std::istream& in, String& out)
+{
+
+	
+	char* buffer = new char[100];
+	memset(buffer, 0, 100);
+	in >> buffer;
+	out = buffer;
+	delete[] buffer;
+	return in;
+}
